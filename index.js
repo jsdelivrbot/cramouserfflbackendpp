bd3e459dfaf34448a114da9b55b4da577604adf4
Jacob Pålsson <jacpa975@student.liu.se>
2017-10-23 00:27:55 +0200

    asd

--- index.js
+++ index.js
@@ -87,6 +87,10 @@ const removeLot = ({id, floor}) => {
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var http = require('http').Server(app);
+var db = require('./models/index.js')
+db.sequelize.sync().then(()=>{
+  
+})
 
 var bodyParser = require('body-parser');
 var cors = require('cors')
@@ -108,44 +112,63 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 router.get('/lots', function(req, res) {
-  res.json(lots);   
+  db.Lot.findAll({include: [{model: db.Floor, as: 'floors'}]}).then(Lots => {
+    res.json(Lots);   
+  })
+  
 });
 router.post('/lots', function(req, res) {
-  res.json(updateLot({lot: req.body.lot}))
-  io.emit('update', { for: 'everyone' })
+  res.json({success: true})
+  let promises = []
+  console.log(req.body.lot)
+  for(let i = 0; i < req.body.lot.floors.length; i++) {
+    let f = req.body.lot.floors[i]
+    let empty = (f.individual === null || typeof f.individual === 'undefined')
+    && (f.type === null || typeof f.type === 'undefined')
+    && (f.status === null || typeof f.status === 'undefined')
+    promises.push(db.Floor.update(
+      {
+      floor: empty ? null : i,
+      individual: f.individual || null,
+      type: f.type || null,
+      status: f.status || null
+    }, {where: {id: f.id}}))
+  }
+  Promise.all(promises).then(() => {
+    io.emit('update', { for: 'everyone' })
+  })
+  
 });
 function isLetter(str) {
   return str.length === 1 && str.match(/[a-z]/i);
 }
 router.get('/addLot/:r/:c', function(req, res) {
   if(isLetter(req.params.r) && !isNaN(Number(req.params.c))) {
-    for(let l of lots) {
-      if(l.col == req.params.c && l.row === req.params.r) {
-        return res.json({success: true})    
+    db.Lot.findOne({where:{col: req.params.c, row: req.params.r}}).then((lot)=>{
+      if(!lot) {
+        db.Lot.create({row: req.params.r, col: req.params.c}).then(newLot => {
+          let promises = [
+            db.Floor.create({LotId: newLot.id}),
+            db.Floor.create({LotId: newLot.id}),
+            db.Floor.create({LotId: newLot.id})
+          ]
+          Promise.all(promises).then(() => {
+            io.emit('update', { for: 'everyone' })
+          })
+        })
       }
-    }
-    lots.push (
-      {
-        id:req.params.c + req.params.r,
-        col: Number(req.params.c),
-        row: req.params.r,
-        floors: [{},{},{}]
-      }
-    )
-    io.emit('update', { for: 'everyone' })
+    })
     return res.json({success: true})
   }
   return res.json({success: false})
 });
 router.get('/removeLot/:r/:c', function(req, res) {
   if(isLetter(req.params.r) && !isNaN(Number(req.params.c))) {
-    for(let i=0;i<lots.length;i++) {
-      if(lots[i].col == req.params.c && lots[i].row === req.params.r) {
-        lots.splice(i, 1)
-        io.emit('update', { for: 'everyone' })
-        return res.json({success: true})    
-      }
-    }
+    db.Lot.destroy({where:{col: Number(req.params.c), row: req.params.r}}).then(() => {
+      io.emit('update', { for: 'everyone' })
+      return res.json({success: false})
+    })
+    
   }
   return res.json({success: false})
 });
